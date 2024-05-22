29aa3501dc518b11129ce5a0d6b16e074a6516f5
kykkyk96 <beastium96@naver.com>
2024-05-22 16:06:52 +0900

    올리기연습

---
+++ Ch5ArrayBasic/src/MultiArrayBasic2.java
@@ -0,0 +1,31 @@
+
+public class MultiArrayBasic2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//2차원배열
+		//
+		int[][] scoreArr = {
+				{
+					1,2,3
+					
+				},
+				{
+					10,50,100
+				}
+		
+		};
+		
+		for (int i = 0; i < scoreArr.length; i++) {
+			
+			for (int j = 0; j < scoreArr[i].length; j++) {
+				System.out.print(scoreArr[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
